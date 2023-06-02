b5d00bb733b91d580c4ce42ac00594e41785536b
mirqodirov2001 <mirqodirov678@gmail.com>
2023-06-02 18:57:34 +0500

    JavaScript

--- lesson - 2/script.js
+++ lesson - 2/script.js
@@ -1,1 +1,11 @@
-console.log(12 / "10");
+//Type conversion
+
+//1. 12+"" // type string;
+//2. var result = String(number);
+
+let res = Number(true);
+
+console.log(false);
+console.log(res);
+
+console.log(typeof String(12));
