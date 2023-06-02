f31b71eea7ee4962c67badca44c237f3e6c5b5d1
mirqodirov2001 <mirqodirov678@gmail.com>
2023-06-02 18:22:39 +0500

    JavaScript start

---
+++ lesson - 1/script.js
@@ -0,0 +1,95 @@
+/*
+//var
+
+var a = 12;      //e'lon qilsak bo'ladi;
+console.log(a);
+
+a=13;            //qiymat tayinlasak bo'ladi;
+console.log(a);
+
+var a = 25;       //qayta e'lon qilsak bo'ladi;
+console.log(a);
+*/
+
+/*
+//let
+
+let a = 35;      //e'lon qilsak bo'ladi;
+console.log(a);
+
+a=50;            //qiymat tayinlasak bo'ladi;
+console.log(a);
+
+let a = 21;
+console.log(a)   //qayta e'lon qilib bo'ladi;
+
+*/
+
+
+/*
+//const - o'zgarmas degan ma'noni bildiradi;
+
+//e'lon qilsak bo'ladi
+const a = 12;         
+              
+//qayta qiymat tayinlab bo'lmaydi;
+a = 13;  
+
+//qayta e'lon qilib bo'lmaydi;
+const a = 14;
+
+console.log(a);
+*/
+
+
+/*
+//string
+console.log(12 - "salom"); //NOT A NAMBER;
+
+console.log(12 / "10");
+
+console.log(12 * "10");
+
+console.log(12 + "12");
+
+console.log(typeof NaN);
+
+*/
+
+
+/*
+//boolean
+
+console.log(12 > 10); //false vs true 
+*/
+
+
+/*
+//indefined
+
+let number // indefined qiymat berilmasa
+
+let number = null; // null o'zgaruvchi qiymati bo'shligi
+
+console.log(number);
+*/
+
+
+/*
+//Symbol
+let person1 = Symbol('Sardor');
+let person2 = Symbol('Sardor');
+
+console.log(person1);
+
+console.log(person1==person2);
+
+*/
+
+/*
+//bigInt
+let num = BigInt(123456734253253425443523545432);
+console.log(typeof num);
+console.log(num);
+console.log(32421421435n);
+*/
